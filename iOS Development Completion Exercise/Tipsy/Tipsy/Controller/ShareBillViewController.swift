990d3ad702b2f449931ee165c163b1fa228ebac3
MahiAlJawad <br.mahialjawad@gmail.com>
2022-05-29 16:43:06 +0600

    partially commited

--- iOS Development Completion Exercise/Tipsy/Tipsy/Controller/ShareBillViewController.swift
+++ iOS Development Completion Exercise/Tipsy/Tipsy/Controller/ShareBillViewController.swift
@@ -1,11 +1,32 @@
 import UIKit
 
 class ShareBillViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var numberOfSplits: UILabel!
+    
+    @IBOutlet weak var stepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func tipTapped(_ sender: Any) {
+        let tipButton = sender as! UIButton
+        let tipText = tipButton.currentTitle
+        
+        switch tipText {
+        case "0%": print("Selected 0%")
+        case "10%": print("Selected 10%")
+        case "20%": print("Selected 20%")
+        default: break
+        }
+    }
+    
+    @IBAction func calculateTapped(_ sender: Any) {
+        
+    }
 }
 
