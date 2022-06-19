77875c1fb0bc902ed718c37b2d4a20bc19b9ca93
MahiAlJawad <br.mahialjawad@gmail.com>
2022-06-19 14:29:36 +0600

    added pod CLTypingLabel

--- iOS Development Completion Exercise/Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ iOS Development Completion Exercise/Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,9 +14,20 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        animateTitle()
     }
     
-
+    func animateTitle() {
+        titleLabel.text = ""
+        
+        let title = "⚡️FlashChat"
+        var index = 0
+        
+        for letter in title {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(index), repeats: false) { _ in
+                self.titleLabel.text?.append(letter)
+            }
+            index += 1
+        }
+    }
 }
